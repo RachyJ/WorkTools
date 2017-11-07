6b031c1d9800b280603e6b1994262dcbb5855009
Rachel J <rachel@dynamsoft.com>
2017-11-07 14:34:56 +0800

    read product name

--- removecsvduplicate.py
+++ removecsvduplicate.py
@@ -3,4 +3,5 @@ import csv
 with open('Download.csv',newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     for row in spamreader:
-        print(row[1])  # retrive the email value
+        print(row[1])  # retrive the email value in download export
+        print(row[3])  # retrieve the product name in download export
