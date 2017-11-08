766103095229f9826b618c1cf4de990ece06c566
Rachel J <rachel@dynamsoft.com>
2017-11-08 18:00:29 +0800

    remove the extra index column

--- removecsvduplicate.py
+++ removecsvduplicate.py
@@ -3,17 +3,18 @@ import pandas as pd
 from sys import argv
 from pandas.io.excel import ExcelWriter
 
-script, infile, outfile = argv  # take arguments from command line
+script, infile = argv  # take arguments from command line
 
 #in_file = 'files/Download.csv'
 #output_file = 'files/2.csv'
 
 in_file = infile
-output_file = outfile
+index = in_file.find('.')
+output_file = in_file[0:index] + '.xlsx' # use the same file name for export
 
 df = pd.read_csv(in_file)
 df.drop_duplicates(subset=['Email','Product'], inplace=True)  # remove duplicates by email + product
 
-with ExcelWriter(outfile) as ew:
-    df.to_excel(ew)
+with ExcelWriter(output_file) as ew:
+    df.to_excel(ew, index=False)  # index=False no extra index column
         
