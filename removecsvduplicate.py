ee69fb4f9ed49370fe5fc0dad79e77fc523b1557
Rachel J <rachel@dynamsoft.com>
2017-11-08 17:40:35 +0800

    auto convert to xlsx successfully

--- removecsvduplicate.py
+++ removecsvduplicate.py
@@ -1,8 +1,9 @@
 import csv
 import pandas as pd
 from sys import argv
+from pandas.io.excel import ExcelWriter
 
-script, infile, outfile = argv
+script, infile, outfile = argv  # take arguments from command line
 
 #in_file = 'files/Download.csv'
 #output_file = 'files/2.csv'
@@ -11,6 +12,8 @@ in_file = infile
 output_file = outfile
 
 df = pd.read_csv(in_file)
-df.drop_duplicates(subset=['Email','Product'], inplace=True)
-df.to_csv(output_file)
+df.drop_duplicates(subset=['Email','Product'], inplace=True)  # remove duplicates by email + product
+
+with ExcelWriter(outfile) as ew:
+    df.to_excel(ew)
         
