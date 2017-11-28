dc16e4510d2f848fbdf9afeb6157f35b186b5e0f
Rachel J <rachel@dynamsoft.com>
2017-11-28 18:19:53 +0800

    remove unneeded columns

--- removecsvduplicate.py
+++ removecsvduplicate.py
@@ -10,6 +10,11 @@ output_file = infile.split('.')[0] + '.xlsx'  # use the same file name as output
 df = pd.read_csv(infile, encoding='ISO-8859-1')
 df.drop_duplicates(subset=['Email','Product'], inplace=True)  # remove duplicates by email + product
 
+columns2delete = set(['Choose','Delete'])
+if columns2delete.issubset(df.columns):
+    for e in columns2delete:
+        del df[e]
+
 with ExcelWriter(output_file) as ew:
     df.to_excel(ew, index=False)  # index=False no extra index column
         
