3f7dbaca6d136213a0d7ba78f12ade23f083aa68
rachel <beirikui@qq.com>
2018-01-22 10:00:10 +0800

    inplace to true

--- removecsvduplicate.py
+++ removecsvduplicate.py
@@ -7,28 +7,24 @@ script, infile = argv  # take arguments from command line
 
 output_file = infile.split('.')[0] + '.xlsx'  # use the same file name as output
 
-df = pd.read_csv(infile, encoding='ISO-8859-1')
-df.drop_duplicates(subset=['Email','Product'], inplace=True)  # remove duplicates by email + product
-
-# columns2delete = set(['Choose','Delete','Pay Date','Order Type','Reseller Country','Total Without Tax','Tax Fee','Upgrade Fee','YS Fee','Late Payment Fee','Technical Contact Email','Technical Contact Name','Labels1'])
-
-# special_columns = set(['Ticket No.','Add-on Fee','Discount (%)','Discount($)'])
-# columns2delete2 = special_columns.filter(regex='Ticket No|Discount|Add on Fee ').columns
+df = pd.read_csv(infile, encoding='ISO-8859-1') # consider index_col=False to force pandas to not use the first column as the index (row names)
+df.drop_duplicates(subset=['Email','Product'], inplace=True)  # remove duplicates by email + product; drop duplicates
 
 columns2delete = set(['Choose','Delete']) # invalid columns
 if columns2delete.issubset(df.columns):
     for e in columns2delete:
         del df[e]
 
+# if df.columns.str.contains('dynamsoft.com'):
+
 order_columns = set(['Pay Date'])
 if order_columns.issubset(df.columns):
-    print('this is the order report')
-    print(df.columns.get_loc('Ticket No.')) # delete from ticket no. column
-    print(len(df.columns)) # get total column number
+
     start_column = df.columns.get_loc('Ticket No.')
     end_column = len(df.columns)
-    col = list(df.columns)[start_column:end_column]
+    col = list(df.columns)[start_column-1:end_column]
     df.drop(col, axis=1, inplace=True)
+
 
 with ExcelWriter(output_file) as ew:
     df.to_excel(ew, index=False)  # index=False no extra index column
